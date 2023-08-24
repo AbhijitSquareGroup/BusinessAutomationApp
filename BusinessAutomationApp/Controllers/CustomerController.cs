ec348f5c092a1960ae4c89b721f9d64be9c93c72
abhijit-ghosh <abhijit@squaregroup.com>
2023-08-24 14:54:57 +0600

    model binding

---
+++ BusinessAutomationApp/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessAutomationApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessAutomationApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Create(CustomerCreate customer)
+        {
+            return $"this is the create page: {customer.Name} phone :{customer.Phone}";
+        }
+    }
+}
---
+++ BusinessAutomationApp/Models/CustomerCreate.cs
@@ -0,0 +1,8 @@
+﻿namespace BusinessAutomationApp.Models
+{
+    public class CustomerCreate
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
